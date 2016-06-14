20141a0e218af332f8d5e979ce85458876366947
Katsuki Kobayashi <rare@tirasweel.org>
2016-06-15 00:01:56 +0900

    implement analysis operation of ftyp-atom, nastily.

--- atom/atom.go
+++ atom/atom.go
@@ -6,26 +6,58 @@ import (
 	"io"
 )
 
-var atom_parsers = map[string]func(r io.Reader) (a Atom){
+var atom_parsers = map[string]func(Atom, io.Reader) ftypAtom{
 	"ftyp": parse_ftyp,
 }
 
 type Atom struct {
 	size  uint32
 	atype string
+	Print func()
 }
 
-func parse_ftyp(r io.Reader) (a Atom) {
+type ftypAtom struct {
+	Atom
+	major_brand       string
+	minor_version     string
+	compatible_brands []string
+}
 
-	var ftyp Atom
+func (fa *ftypAtom) Print() {
+
+	fmt.Printf("size: %d\n", fa.size)
+	fmt.Printf("type: %s\n", fa.atype)
+	fmt.Printf("Major_brand: %s\n", fa.major_brand)
+	fmt.Printf("Minor_version: %s\n", fa.minor_version)
+
+	for _, v := range fa.compatible_brands {
+		fmt.Printf("Compatible_brands: %s\n", v)
+	}
+}
+
+func parse_ftyp(a Atom, r io.Reader) ftypAtom {
+
+	var ftyp ftypAtom
+
+	ftyp.size = a.size
+	ftyp.atype = a.atype
+
+	buf := make([]byte, 4)
+
+	r.Read(buf)
+	ftyp.major_brand = string(buf)
+
+	r.Read(buf)
+	ftyp.minor_version = string(buf)
+
+	remain_size := ftyp.size - 16
+
+	for i := uint32(0); i < remain_size; i += 4 {
+		r.Read(buf)
+		ftyp.compatible_brands = append(ftyp.compatible_brands, string(buf))
+	}
 
 	return ftyp
-}
-
-func Print_atom(a Atom) {
-
-	fmt.Printf("size: %d\n", a.size)
-	fmt.Printf("type: %s\n", a.atype)
 }
 
 func (a *Atom) Parse(r io.Reader) {
@@ -43,5 +75,7 @@ func Parse_atom(r io.Reader) {
 	r.Read(buf)
 	atom.atype = string(buf)
 
-	Print_atom(atom)
+	mp4 := atom_parsers[atom.atype](atom, r)
+
+	mp4.Print()
 }
